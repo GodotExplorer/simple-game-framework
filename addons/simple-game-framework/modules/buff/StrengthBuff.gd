acaae830a877612cc23ff5075c4df0346c5c33ba
Geequlim <geequlim@gmail.com>
2019-05-29 18:31:35 +0800

    添加带强度的Buff

--- addons/simple-game-framework/modules/buff/StrengthBuff.gd
+++ addons/simple-game-framework/modules/buff/StrengthBuff.gd
@@ -24,45 +24,21 @@
 # SOFTWARE.                                                                      #
 ##################################################################################
 
-# @class `TimerBuff`
-# TimerBuff 类
-# 限时Buff， 指定时刻之前有效，超时自动停止效果
-# 时间指的是真实时间，因此不受游戏暂停或玩家离线等条件的影响，超过指定时间后失效
-
-tool
+# @class `StrengthBuff`
+# StrengthBuff 类
+# 带强度的 Buff， `strength` 衰减小于等于0时失效
 extends Buff
-class_name TimerBuff
+class_name StrengthBuff
 
-# 剩余时间(秒)
-var time_left = 0 setget set_time_left, get_time_left
+# 强度值
+var strength = 0
 
-# 设定时长，启动后修改无效
-var duration = 5
-
-# 结束时刻
-var _timeout: float = 0
-
-# 设置剩余时间(秒)
-func set_time_left(v: float):
-	self._timeout = now() + v
-
-# 获取剩余时间(秒)
-func get_time_left() -> float:
-	return self._timeout - now()
-
-func on_start():
-	self.time_left = duration
-
-func update(dt: float):
-	if now() >= self._timeout:
+# 衰减强度
+func attenuate(amount = 1):
+	self.strength -= amount
+	if strength <= 0:
 		stop()
 
-# 当前时间点（秒）
-func now() -> float:
-	return OS.get_system_time_msecs() / 1000.0
-
-func save() -> Dictionary:
-	return { timeout = self._timeout }
-
-func load(data: Dictionary):
-	self._timeout = data.timeout
+func update(dt):
+	if strength <= 0:
+		stop()
